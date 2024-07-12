4e22e340bb45f2f5f38f4952c1eccf8c1a516fbd
aditya-12 <adityakaushik2011@gmail.com>
2024-07-12 13:37:09 -0500

    sidebar conversation hover color change.

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -11,7 +11,7 @@ const Conversation = ({ conversation, lastIdx }) => {
 
 
     return <>
-        <div className={`flex gap-2 items-center rounded p-2 py-2 cursor-pointer
+        <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-2 cursor-pointer
             ${isSelected ? "bg-sky-500" : ""}`}
             onClick={() => setSelectedConversation(conversation)}>
                 
